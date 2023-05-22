23b49a112a8471b527d06389beef766235332d5f
Marcelo Valada <marcelovalada2@hotmail.com>
2023-05-22 20:10:41 -0300

    Add Survey controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,45 @@
 class SurveysController < ApplicationController
+  before_action :set_survey, only: [:show, :update, :destroy]
+
+  def index
+    @surveys = Survey.all
+    render json: @surveys
+  end
+
+  def show
+    render json: @survey
+  end
+
+  def create
+    @survey = Survey.new(survey_params)
+
+    if @survey.save
+      render json: @survey, status: :created
+    else
+      render json: @survey.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @survey.update(survey_params)
+      render json: @survey
+    else
+      render json: @survey.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @survey.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_survey
+    @survey = Survey.find(params[:id])
+  end
+
+  def survey_params
+    params.require(:survey).permit(:title, questions_attributes: [:content, answers_attributes: [:content]])
+  end
 end
