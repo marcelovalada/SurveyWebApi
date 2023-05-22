30c0d97177e5b02228818838d94e7c103c522f46
Marcelo Valada <marcelovalada2@hotmail.com>
2023-05-22 18:29:39 -0300

    Create Survey, Question and Answer Models

---
+++ app/models/question.rb
@@ -0,0 +1,6 @@
+class Question < ApplicationRecord
+  belongs_to :survey
+  has_many :answers, dependent: :destroy
+  accepts_nested_attributes_for :answers, allow_destroy: true
+  validates :content, presence: true
+end
