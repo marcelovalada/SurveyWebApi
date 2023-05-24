6e669057aacf523a865c92e86f6d2ce0ffc8f0ef
Marcelo Valada <marcelovalada2@hotmail.com>
2023-05-24 01:19:14 -0300

    Create seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   acts_as_token_authenticatable
+
+  validates_inclusion_of :admin, in: [true, false]
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :admin, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+coordinator = User.create!(
+  email: 'coordinator@user',
+  password: '123456',
+  admin: true
+)
+
+respondent = User.create!(
+  email: 'respondent@user',
+  password: '123456',
+  admin: false
+)
+
+puts 'Usuários Admin e Comum criados com sucesso!'
